220c3a6d94a7f8774ec27fd5905dab084206d28f
zachary <RyanLiuF@126.com>
2019-05-07 10:13:58 +0800

    add the files

---
+++ client/app/core/components/header/logo.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+	.logo
+		a.nav-item(href="#")
+			span 
+				strong FAN
+				| AI App
+</template>
+
+<script>
+
+	export default {
+
+	};
+	
+</script>
+
+<style lang="scss">
+</style>
